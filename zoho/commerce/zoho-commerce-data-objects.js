e14d84455435b02cc4d2708728402397e4e741c2
Tom Black <tom@codelasso.com>
2023-12-24 18:34:27 -0500

    remove old pricing

--- zoho/commerce/zoho-commerce-data-objects.js
+++ zoho/commerce/zoho-commerce-data-objects.js
@@ -12,10 +12,10 @@ window.zs_product = {
     "has_variant_price": false,
     "has_variants": false,
     "is_out_of_stock": false,
-    "label_price": 15.17,
-    "selling_price": 15.17,
-    "starts_with": 15.17,
-    "ends_with": 15.17,
+    "label_price": 0,
+    "selling_price": 0,
+    "starts_with": 0,
+    "ends_with": 0,
     "on_sale": false,
     "is_returnable": true,
     "is_stock_managed": false,
@@ -33,7 +33,7 @@ window.zs_product = {
             "stock_available": 1,
             "is_out_of_stock": false,
             "is_returnable": false,
-            "selling_price": 15.17,
+            "selling_price": 0,
             "label_price": 0,
             "sku": "1002", "isbn": "", "mpn": "", "upc": "", "ean": "", "price_brackets": [], "images": [{ "id": "-1", "url": "/zs-common/images/no-preview-image.jpg", "title": "", "alternate_text": "", "order": 1, "is_featured": true, "is_placeholder_image": true }], "custom_fields": [], "is_available_for_purchase": true, "is_deliverable": true }], "tags": [], "bread_crumbs": [{ "name": "Piccolo", "url": "/categories/piccolo/3228418000004564172", "is_selected": false }, { "name": "Bach 196 Bb/A Piccolo Trumpet Short Bell - Basic Valve Guard - Black", "url": "/products/b14201880b/3228418000000212529", "is_selected": false }], "review_id": "1545177000000023033", "is_social_share_enabled": false, "is_product_custom_fields_enabled": false, "is_product_review_enabled": false, "is_product_price_brackets_available": false, "is_input_custom_field_available": false, "unit": "pcs", "is_available_for_purchase": true, "is_deliverable": true, "embed_js_url": "https://sites-stratus.zohostratus.com/IDC/js/zstore-embed.js", "embed_css_url": "https://sites-stratus.zohostratus.com/IDC/css/zstore-embed.css"
 };
